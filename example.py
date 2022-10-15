54a4e59cee2f9af0b4091da4ef7d9f801f169e86
Alec Petersen <k.alecpetersen@gmail.com>
2022-10-15 09:34:10 -0400

    MPY-11 Update example usage

--- example.py
+++ example.py
@@ -5,12 +5,22 @@ with mud.MudFile(r"/Users/apetersen/PycharmProjects/MudPy/006523.msr", 'r') as m
     print(mf.get_subtitle())
     print(mf.get_header_comments())
     print(mf.get_comments())
-
     print("\n### Getting scalars ###")
     print(mf.get_scalers())
 
     print("\n### Getting independent variables ###")
     print(mf.get_independent_variables())
+
+    print("\n### Getting temp and field")
+    print(mf.get_temperature())
+    print(mf.get_field())
+
+    print("\n### Getting histograms")
+    histograms = mf.get_histograms()
+    print(histograms['Forw'])
+    print(histograms[5])
+
+
 
 
 
