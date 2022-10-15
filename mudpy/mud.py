8d3b1aec5fcd7120628f19532ddfd25bc5a94a3e
Alec Petersen <k.alecpetersen@gmail.com>
2022-10-15 09:36:20 -0400

    MPY-11 Fill out stubs to call cmud methods

--- mudpy/mud.py
+++ mudpy/mud.py
@@ -1,6 +1,7 @@
 """Provides a friendly interface to the C Mud library."""
 import os
 import logging
+import re
 
 from mudpy import cmud
 
@@ -59,63 +60,108 @@ class MudFile:
     def __exit__(self, exc_type, exc_val, exc_tb):
         cmud.close_read(self.__cmud_file_handle)
 
-    def get_run_description(self) -> str:
-        raise NotImplementedError()
+    def get_run_description(self) -> cmud.RunDescription:
+        """Returns the complete run description."""
+        return cmud.get_run_desc(self.__cmud_file_handle, self.__default_string_buffer_size)
 
     def get_experiment_number(self) -> int:
-        raise NotImplementedError()
+        """Returns the experiment number field."""
+        return cmud.get_expt_number(self.__cmud_file_handle)[1]
 
     def get_run_number(self) -> int:
-        raise NotImplementedError()
+        """Returns the run number field."""
+        return cmud.get_run_number(self.__cmud_file_handle)[1]
 
     def get_elapsed_seconds(self) -> int:
-        raise NotImplementedError()
+        """Returns the elapsed seconds field."""
+        return cmud.get_elapsed_seconds(self.__cmud_file_handle)[1]
 
     def get_time_begin(self) -> int:
-        raise NotImplementedError()
+        """Returns the time begin field."""
+        return cmud.get_time_begin(self.__cmud_file_handle)[1]
 
     def get_time_end(self) -> int:
-        raise NotImplementedError()
+        """Returns the time end field."""
+        return cmud.get_time_end(self.__cmud_file_handle)[1]
 
     def get_title(self) -> str:
-        raise NotImplementedError()
+        """Returns the title field."""
+        return cmud.get_title(self.__cmud_file_handle, self.__default_string_buffer_size)[1]
 
     def get_lab(self) -> str:
-        raise NotImplementedError()
+        """Returns the lab field."""
+        return cmud.get_lab(self.__cmud_file_handle, self.__default_string_buffer_size)[1]
 
     def get_area(self) -> str:
-        raise NotImplementedError()
+        """Returns the area field."""
+        return cmud.get_area(self.__cmud_file_handle, self.__default_string_buffer_size)[1]
 
     def get_method(self) -> str:
-        raise NotImplementedError()
+        """Returns the method field."""
+        return cmud.get_method(self.__cmud_file_handle, self.__default_string_buffer_size)[1]
 
     def get_apparatus(self) -> str:
-        raise NotImplementedError()
+        """Returns the apparatus field."""
+        return cmud.get_apparatus(self.__cmud_file_handle, self.__default_string_buffer_size)[1]
 
     def get_insert(self) -> str:
-        raise NotImplementedError()
+        """Returns the insert field."""
+        return cmud.get_insert(self.__cmud_file_handle, self.__default_string_buffer_size)[1]
 
     def get_sample(self) -> str:
-        raise NotImplementedError()
+        """Returns the sample field."""
+        return cmud.get_sample(self.__cmud_file_handle, self.__default_string_buffer_size)[1]
 
     def get_orientation(self) -> str:
-        raise NotImplementedError()
+        """Returns the orientation of the sample."""
+        return cmud.get_orient(self.__cmud_file_handle, self.__default_string_buffer_size)[1]
 
     def get_das(self) -> str:
-        raise NotImplementedError()
+        """Returns the das (data acquisition system) field."""
+        return cmud.get_das(self.__cmud_file_handle, self.__default_string_buffer_size)[1]
 
     def get_experimenters(self) -> str:
-        raise NotImplementedError()
+        """Returns the experimenter field."""
+        return cmud.get_experimenter(self.__cmud_file_handle, self.__default_string_buffer_size)[1]
 
-    def get_temperature(self) -> float:
-        raise NotImplementedError()
+    def get_temperature(self) -> tuple:
+        """Returns the temperature, with units."""
+        temperature_with_units = cmud.get_temperature(self.__cmud_file_handle, self.__default_string_buffer_size)[1]
+        temp_value = re.search(r"(\d*\.*\d*)", temperature_with_units)
+        temp_units = re.search(r"([a-zA-Z]+)", temperature_with_units)
 
-    def get_field(self) -> float:
-        raise NotImplementedError()
+        if len(temp_value.groups()) == 0:
+            return None, None
+        value = temp_value.group(0)
+
+        if len(temp_units.groups()) == 0:
+            return value, None
+        units = temp_units.group(0)
+
+        return value, units
+
+    def get_field(self) -> tuple:
+        """Returns the magnetic field, with units."""
+        field_with_units = cmud.get_field(self.__cmud_file_handle, self.__default_string_buffer_size)[1]
+        field_value = re.search(r"(\d*\.*\d*)", field_with_units)
+        field_units = re.search(r"([a-zA-Z]+)", field_with_units)
+
+        if len(field_value.groups()) == 0:
+            return None, None
+        value = field_value.group(0)
+
+        if len(field_units.groups()) == 0:
+            return value, None
+        units = field_units.group(0)
+
+        return value, units
 
     def get_independent_variables(self):
         """Returns the independent variables, if any, for the file."""
         _, _, num_variables = cmud.get_ind_vars(self.__cmud_file_handle)
+
+        if num_variables is None:
+            return []
 
         return [
             cmud.get_ind_var(self.__cmud_file_handle, i, self.__default_string_buffer_size)
@@ -125,6 +171,9 @@ class MudFile:
     def get_scalers(self) -> list:
         """Returns the scalers, if any, for the file."""
         _, _, num_scalers = cmud.get_scalers(self.__cmud_file_handle)
+
+        if num_scalers is None:
+            return []
 
         return [
             cmud.get_scaler(self.__cmud_file_handle, i, self.__default_string_buffer_size)
@@ -149,10 +198,13 @@ class MudFile:
         Note that this does not include header comments."""
         _, _, num_comments = cmud.get_comments(self.__cmud_file_handle)
 
+        if num_comments is None:
+            return []
+
         return [
             cmud.get_comment(self.__cmud_file_handle, i, self.__default_string_buffer_size, body_buffer_size)
             for i in range(1, num_comments + 1)
         ]
 
-    def get_histograms(self) -> HistogramCollection:
-        raise NotImplementedError()
+    def get_histograms(self) -> cmud.HistogramCollection:
+        return cmud.get_histogram_collection(self.__cmud_file_handle, self.__default_string_buffer_size)
